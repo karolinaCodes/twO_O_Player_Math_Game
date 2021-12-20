5f1a8ffdf63e443834e0a473bfd8809a7da02e0e
Karolina Swislocki <karolinaswislocki@hotmail.com>
2021-12-20 14:29:24 -0500

    refactor Game class to follow ruby best practices

--- game.rb
+++ game.rb
@@ -1,29 +1,30 @@
  class Game
-  attr_accessor :curr_player
+  attr_accessor :curr_player, :player1, :player2
 
-  def instantiate
+  def instantiate player1, player2
     @curr_player = 0
-    @players= ["Player 1", "Player2"]
+    @player1 = player1
+    @player2 = player2
   end
 
   def next_turn
     puts "NEW TURN"
     @curr_player += 1
     if (@curr_player % 2 === 0)
-      puts "#{players[1]}'s Turn"
+      puts "#{player1}'s Turn"
     else
-      puts "#{players[0]}'s Turn"
+      puts "#{player2}'s Turn"
     end
  end
 
  def current_score
-  puts "P1 #{} vs P2 #{}"
+  puts "P1 #{self.player1.score} vs P2 #{self.player2.score}"
  end
 
  def ending_message
-  if p1.score == 0
+  if player1.score == 0
     puts "Player 2 is the winner!"
-  elsif p2.score == 0
+  elsif player2.score == 0
     puts "Player 1 is the winner!"
   end
 
