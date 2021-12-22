054c817fd0e176ab599e5837a0214b2e7955b34d
Karolina Swislocki <karolinaswislocki@hotmail.com>
2021-12-22 12:16:39 -0500

    add instance varaible @name

--- player.rb
+++ player.rb
@@ -1,10 +1,10 @@
 class Player
-  attr_accessor :lives
+  attr_accessor :lives, :name, :id
 
-  def initialize name
+  def initialize name, id
+    @id = id
     @name = name
-    @lives= 3
-    @score = 0 
+    @lives = 3
   end
 
   def lose_life
